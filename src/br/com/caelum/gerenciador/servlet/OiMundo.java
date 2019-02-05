6626c7509e3df34eb4c83d8711bd4aa1304b4c51
Regis Ribeiro <rockmaior@gmail.com>
2019-02-05 11:18:40 -0200

    inicializacao do projeto contendo os fontes das tres primeiras aulas do curso servlet pt 1 - alura

---
+++ src/br/com/caelum/gerenciador/servlet/OiMundo.java
@@ -0,0 +1,24 @@
+package br.com.caelum.gerenciador.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundo extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("Parabéns, você escreveu sua primeira servlet");
+		out.println("</body>");
+		out.println("</html>");
+		System.out.println("A servlet foi chamada");
+	}
+}
